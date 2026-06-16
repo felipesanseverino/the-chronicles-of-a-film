6725583719f7cde1484962a2957faa3de38d4f01
Felipe <felipemarti@gmail.com>
2026-06-16 09:52:09 +0200

    Resolve chapter photo folders automatically

--- config.js
+++ config.js
@@ -211,11 +211,7 @@ const series = [
       "FOMAPAN ASA 400 - 120MM0006.jpg",
       "1000127166.jpg",
       "1000127177.jpg",
-    ],
-    photoFolders: {
-      "1000127166.jpg": "chronicles/at-anchor",
-      "1000127177.jpg": "chronicles/at-anchor",
-    }
+    ]
   },
   {
     slug: "slovenia",
